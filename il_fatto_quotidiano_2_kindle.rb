b9738dec1c996979d42ea129af6efa395ff1768e
Antonio Carpentieri <a.carpe@gmail.com>
2010-11-15 17:31:42 +0100

    Switched to a solution entirely based on mechanize to send the email

--- il_fatto_quotidiano_2_kindle.rb
+++ il_fatto_quotidiano_2_kindle.rb
@@ -2,52 +2,59 @@ require 'rubygems'
 require 'net/http'
 require 'mechanize'
 require 'yaml'
-require File.join(Dir.pwd,'il_fatto_quotidiano_mailer.rb')
 
 class IlFattoQuotidiano2Kindle
   def self.send
     config = YAML::load_file(File.join(Dir.pwd,"config.yml" ) )
-    setting_up_smtp(config)
     begin
       agent = Mechanize.new { |agent|
         agent.user_agent_alias = 'Mac Safari'
       }
 
-      puts "#{Time.now.to_s} - logging in on ilfatto..."
+      log("logging in on ilfatto...")
       agent.get(config['site_login_url']) do |page|
         login = page.form_with(:name => 'loginform')
         login.log = config['site_user_name']
         login.pwd = config['site_password']
         login.submit
       end
-      puts "#{Time.now.to_s} - login done"
+      log("login done")
       date=Time.now.strftime("%Y%m%d")
       file_name = "/tmp/ilfatto#{date}.pdf"
-      puts "#{Time.now.to_s} - getting pdf and saving in /tmp..."
+      log("getting pdf and saving in /tmp...")
       agent.get("#{config['site_hostname']}/openpdf/?n=#{date}").save_as(file_name)
-      puts "#{Time.now.to_s} - done."
-      puts "#{Time.now.to_s} - sending pdf to #{config['free_kindle_email']}..."
-      IlFattoQuotidianoMailer.file(config['free_kindle_email'],config['user_email'], file_name, "application/pdf").deliver
-      puts "#{Time.now.to_s} - done."
+      log("done.")
+      log("logging into Gmail to send the email")
+      page = agent.get("http://www.gmail.com")
+      login_form = page.forms.first
+      login_form.Email = config['smtp_user_name']
+      login_form.Passwd = config['smtp_password']
+      home = agent.submit(login_form, login_form.buttons.first)
+      log("logged into Gmail")
+      log("loading HTML Gmail interface")
+      page = agent.get(home.search("//meta").first.attributes['content'].to_s.gsub(/0; url=/,'').gsub(/'/,''))
+      page = agent.get(page.uri.to_s.sub(/\?.*$/, "?ui=html&zy=n"))
+      log("HTML Gmail interface loaded")
+      log("composing email...")
+      page = agent.click(page.links.find { |l| l.text =~ /compose/i })
+      form = page.forms[1]
+      form.to = config['free_kindle_email']
+      form.subject = "ilfatto#{date}"
+      form.file_uploads.first.file_name = file_name
+      form.body = "ilfatto#{date}"
+      page = agent.submit(form, form.buttons.first)
+      log("email sent")
+      log("done.")
     rescue Exception => e
-      puts "#{Time.now.to_s} - #{e.message}"
-      puts "#{Time.now.to_s} - #{e.backtrace.inspect}"
+      log("#{e.message}")
+      log("#{e.backtrace.inspect}")
     end
   end
 
-  def self.setting_up_smtp config
-    ActionMailer::Base.delivery_method = :sendmail
-    # ActionMailer::Base.delivery_method = :smtp
-    # ActionMailer::Base.smtp_settings = {
-        # :enable_starttls_auto => true,
-        # :address => "smtp.gmail.com",
-        # :port => "587",
-        # :domain => "gmail.com",
-        # :authentication => :plain,
-        # :user_name => config['smtp_user_name'],
-        # :password => config['smtp_password']
-      # }
+  def self.log(message)
+     puts "#{Time.now.to_s} - #{message}"
   end
+
 end
 
 IlFattoQuotidiano2Kindle.send
